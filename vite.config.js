12aab8a318dffde2293e60361b2df36c9747d67a
Avyaan Verma <avyaanver@gmail.com>
2024-04-14 22:30:46 +0530

    links updated

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   server:{
     proxy:{
       '/api' : {
-        target: 'http://localhost:8000',
+        target: 'https://crypto-tracker-backend-ccx0.onrender.com/',
         changeOrigin: true
       },
       '/profile' : {
-        target: 'http://localhost:8000',
+        target: 'https://crypto-tracker-backend-ccx0.onrender.com/',
         changeOrigin: true
       }
     },
